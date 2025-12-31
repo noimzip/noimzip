131ce483ec3197e6e677f6613084ff9e74783f98
noimzip <gudoumaki@proton.me>
2025-12-31 12:29:13 +0900

    Add glowing logo

--- src/App.vue
+++ src/App.vue
@@ -10,33 +10,25 @@ import { LiquidGlass } from '@wxperia/liquid-glass-vue'
       :style="{ position: 'fixed', top: '50px', left: '50%', width: '90%'}"
     >
       <div class="p-6">
-        <h2>aaaaaa</h2>
+        <a href="https://github.com/noimzip" target="_blank">
+          <img src="./assets/github-mark-white.svg" class="logo" width="50" height="50" alt="Github logo" />
+        </a>
+        <a href="https://x.com/dZ1ytwQAqUyCT9" target="_blank">
+          <img src="./assets/logo.svg" class="logo" width="50" height="50" alt="X logo" />
+        </a>
       </div>
     </LiquidGlass>
   </header>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld msg="Vite + Vue" />
 
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
   will-change: filter;
-  transition: filter 300ms;
+  transition: filter 500ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 0.5em #fff);
 }
 </style>

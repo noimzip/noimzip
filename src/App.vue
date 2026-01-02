7eff72b51a6455fe96754e2df0c4d29d24b3a993
noimzip <74817397+noimzip@users.noreply.github.com>
2026-01-02 07:56:51 +0000

    Liquid Glass button implementation

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import { LiquidGlass } from '@wxperia/liquid-glass-vue'
 </script>
 
 <template>
   <header>
     <LiquidGlass
-      padding="0px 46%"
-      :style="{ position: 'fixed', top: '50px', left: '50%', width: '90%'}"
+      padding="0px 50%"
+      :style="{ position: 'fixed', top: '50px', left: '50%', width: '90%' }"
+      :elasticity="0.05"
     >
-      <div class="p-6">
-        <a href="https://github.com/noimzip" target="_blank">
-          <img src="./assets/github-mark-white.svg" class="logo" width="50" height="50" alt="Github logo" />
-        </a>
-        <a href="https://x.com/dZ1ytwQAqUyCT9" target="_blank">
-          <img src="./assets/logo.svg" class="logo" width="50" height="50" alt="X logo" />
-        </a>
-      </div>
+      <nav id="header-navbar">
+        <h2>
+          Noimzip
+        </h2>
+        <div id="app-icons">
+          <a href="https://github.com/noimzip" target="_blank">
+            <img src="./assets/github-mark-white.svg" class="logo" width="45" height="45" alt="Github logo" />
+          </a>
+          <a href="https://x.com/dZ1ytwQAqUyCT9" target="_blank">
+            <img src="./assets/logo.svg" class="logo" width="45" height="45" alt="X logo" />
+          </a>
+        </div>
+      </nav>
     </LiquidGlass>
   </header>
-  <HelloWorld msg="Vite + Vue" />
-
+    <LiquidGlass
+      padding="16px"
+      @click="handleClick"
+      :style="{ position: 'fixed', top: '50px', left: '340px' }"
+    >
+      <span style="font-size: 32px; font-weight: 600;">Home</span>
+    </LiquidGlass>
+    <LiquidGlass
+      padding="16px"
+      @click="handleClick"
+      :style="{ position: 'fixed', top: '50px', left: '485px' }"
+    >
+      <span style="font-size: 32px; font-weight: 600;">Gallery</span>
+    </LiquidGlass>
+    <LiquidGlass
+      padding="16px"
+      @click="handleClick"
+      :style="{ position: 'fixed', top: '50px', left: '635px' }"
+    >
+      <span style="font-size: 32px; font-weight: 600;">Profile</span>
+    </LiquidGlass>
 </template>
 
 <style scoped>
 .logo {
   will-change: filter;
-  transition: filter 500ms;
+  transition: filter 250ms;
 }
 .logo:hover {
   filter: drop-shadow(0 0 0.5em #fff);
